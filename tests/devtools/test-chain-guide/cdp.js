187d9b2ff36a2e49bd4f51a0f2f3428b43b7c7d5
Restyled.io <commits@restyled.io>
2020-05-05 09:38:52 +0000

    Restyled by clang-format

--- tests/devtools/test-chain-guide/cdp.js
+++ tests/devtools/test-chain-guide/cdp.js
@@ -4,32 +4,24 @@ const ETH = require('@makerdao/dai-plugin-mcd').ETH;
 const MDAI = require('@makerdao/dai-plugin-mcd').MDAI;
 
 async function start() {
-	try {
-		const maker = await Maker.create('test', {
-			plugins: [
-				[McdPlugin, {}]
-			]
-		});
-		await maker.authenticate();
+  try {
+    const maker = await Maker.create('test', {plugins : [ [ McdPlugin, {} ] ]});
+    await maker.authenticate();
 
-		const balance = await maker
-			.service('token')
-			.getToken('ETH')
-			.balance();
-		console.log('Account: ', maker.currentAddress());
-		console.log('Balance', balance.toString());
+    const balance = await maker.service('token').getToken('ETH').balance();
+    console.log('Account: ', maker.currentAddress());
+    console.log('Balance', balance.toString());
 
-		const cdp = await maker
-			.service('mcd:cdpManager')
-			.openLockAndDraw('ETH-A', ETH(1), MDAI(20));
+    const cdp = await maker.service('mcd:cdpManager')
+                    .openLockAndDraw('ETH-A', ETH(1), MDAI(20));
 
-		console.log('Opened CDP #'+cdp.id);
-		console.log('Collateral amount:'+cdp.collateralAmount.toString());
-		console.log('Debt Value:'+cdp.debtValue.toString());
+    console.log('Opened CDP #' + cdp.id);
+    console.log('Collateral amount:' + cdp.collateralAmount.toString());
+    console.log('Debt Value:' + cdp.debtValue.toString());
 
-		return(cdp.debtValue.toString());
-	} catch (error) {
-		console.log('error', error);
-	}
+    return (cdp.debtValue.toString());
+  } catch (error) {
+    console.log('error', error);
+  }
 }
 module.exports = start;
